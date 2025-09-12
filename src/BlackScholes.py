ed1afd4c3129b71cfb9338ac356edb0b116b371b
Kevin Xiao <kevinxiao2901@gmail.com>
2025-09-12 21:21:48 +1000

    made a heatmap function in BlackScholes and added a description to app.py

--- src/BlackScholes.py
+++ src/BlackScholes.py
@@ -1,5 +1,6 @@
-from numpy import exp, sqrt, log
+from math import exp, sqrt, log
 import numpy as np
+import streamlit as st
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,39 +12,42 @@ class BlackScholes:
     self.time = time
     self.interest_rate = interest_rate
     self.volatility = volatility
-    
-  def calculate_prices(self) -> tuple[float, float]:
-    S = self.current_price
+
+  def calculate_prices(self, S, sigma) -> tuple[float, float]:
     K = self.strike_price
     t = self.time
     r = self.interest_rate
-    sigma = self.volatility
     d1 = (log(S / K) + (r + (sigma ** 2) / 2) * t) / (sigma * sqrt(t))
     d2 = d1 - (sigma * sqrt(t))
     call_price = S * norm.cdf(d1) - K * exp(-r * t) * norm.cdf(d2)
-    put_price = K * exp(-r * t) * norm.cdf(-d2) - S * norm.cdf(-d1) 
+    put_price = K * exp(-r * t) * norm.cdf(-d2) - S * norm.cdf(-d1)
     return call_price, put_price
-  
+
   def heatmap(self):
-    """Heatmap displays current price over volatility"""
-    current_price_range = np.linspace(start=self.current_price*0.5, stop=self.current_price*1.5, num=10)
-    sigma_range = np.linspace(start=self.volatility*0.5, stop=self.volatility*1.5, num=10)
+    call_2darray = np.zeros((10, 10))
+    put_2darray = np.zeros((10, 10))
+    S_range = np.linspace(0.8*self.current_price, 1.2*self.current_price, 10)
+    sigma_range = np.linspace(0.8*self.volatility, 1.2*self.volatility, 10)
     
-    current_price_grid, sigma_grid = np.meshgrid(current_price_range, sigma_range)
-    # values = tuple(zip(current_price_grid.ravel(), sigma_grid.ravel()))
+    for i, S in enumerate(S_range):
+      for j, sigma in enumerate(sigma_range):
+        call, put = self.calculate_prices(S, sigma)
+        call_2darray[i][j] = call
+        put_2darray[i][j] = put
     
-    fig_call, ax_call = plt.subplots(figsize=(10, 8))
-    sns.heatmap(current_price_grid, xticklabels=np.round(current_price_range, 2), yticklabels=np.round(sigma_range, 2), annot=True, fmt=".2f", cmap="viridis", ax=ax_call)
-    ax_call.set_title('CALL')
-    ax_call.set_xlabel('Spot Price')
-    ax_call.set_ylabel('Volatility')
     
-    # Plotting Put Price Heatmap
-    fig_put, ax_put = plt.subplots(figsize=(10, 8))
-    sns.heatmap(sigma_grid, xticklabels=np.round(current_price_range, 2), yticklabels=np.round(sigma_range, 2), annot=True, fmt=".2f", cmap="viridis", ax=ax_put)
-    ax_put.set_title('PUT')
-    ax_put.set_xlabel('Spot Price')
-    ax_put.set_ylabel('Volatility')
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+    sns.heatmap(call_2darray, cmap="viridis", annot=True, ax=axes[0], fmt=".2f", annot_kws={"size": 5})
+    axes[0].set_title("Call Values")
+    axes[0].set_xlabel('Spot Price')
+    axes[0].set_ylabel('Volatility')
     
-    plt.show()
-    return fig_call, fig_put
+    sns.heatmap(put_2darray, cmap="viridis", annot=True, ax=axes[1], fmt=".2f", annot_kws={"size": 5})
+    axes[1].set_title("Put Values")
+    axes[1].set_xlabel('Spot Price')
+    axes[1].set_ylabel('Volatility')
+    
+    st.pyplot(fig)
+    
+    return call_2darray, put_2darray
--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 from BlackScholes import BlackScholes
 
 def main():
@@ -8,27 +7,43 @@ def main():
     page_icon="📈",
     layout="wide")
   st.title("Black-Scholes Option Pricing")
-  col1, col2 = st.columns([1, 3])
   
+  col1, col2, col3, col4, col5 = st.columns(5)
   with col1:
     current_price = st.number_input("Current Asset Price", value=100.0)
-    strike = st.number_input("Strike Price", value=100.0)
-    time = st.number_input("Time to Maturity (Years)", value=1.0)
-    volatility = st.number_input("Volatility (σ)", value=0.2)
-    interest_rate = st.number_input("Risk-Free Interest Rate", value=0.05)
-    
   with col2:
-    model = BlackScholes(current_price, strike, time, interest_rate, volatility)
-    call, put = model.calculate_prices()
-    c1, c2 = st.columns(2)
-    with c1: 
-      st.metric("Call", f"{call:.2f}")
-    with c2: 
-      st.metric("Put", f"{put:.2f}")
+    strike = st.number_input("Strike Price", value=100.0)
+  with col3:
+    time = st.number_input("Time to Maturity (Years)", value=1.0)
+  with col4:
+    volatility = st.number_input("Volatility (σ)", value=0.2)
+  with col5:
+    interest_rate = st.number_input("Risk-Free Interest Rate", value=0.05)
+
+  model = BlackScholes(current_price, strike, time, interest_rate, volatility)
+  call, put = model.calculate_prices(current_price, volatility)
+  c1, c2 = st.columns(2)
+  with c1: 
+    st.metric("Call", f"{call:.2f}")
+  with c2: 
+    st.metric("Put", f"{put:.2f}")
     
-    st.subheader("Options Price - Interactive Heatmap")
-    st.write("Explore how option prices fluctuate with varying 'Spot Prices and Volatility' levels using interactive heatmap parameters, all while maintaining a constant 'Strike Price'.")
-    st.write(model.heatmap())
+  st.subheader("Options Price - Heatmap")
+  model.heatmap()
+
+  # Explanation of the Black_Scholes Equation
+  st.latex(r"""
+  C = N(d_1) S_t - N(d_2) K e^{-rt}
+  """)
+
+  st.latex(r"""
+  where \: d_1 = \frac{\ln \frac{S_t}{K} + \left(r + \tfrac{1}{2}\sigma^2\right)t}{\sigma \sqrt{t}}
+  """)
+
+  st.latex(r"""
+  and \: d_2 = d_1 - \sigma \sqrt{t}
+  """)
+
 
 if __name__ == "__main__":
   main()
