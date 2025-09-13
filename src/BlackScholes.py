ae8a36ff85727619b03b9467d51d10d0af03a316
Kevin Xiao <kevinxiao2901@gmail.com>
2025-09-13 21:30:42 +1000

    make indents 2 spaces into 4 spaces

--- src/BlackScholes.py
+++ src/BlackScholes.py
@@ -5,68 +5,68 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 class BlackScholes:
-  def __init__(self, current_price, strike_price, time, interest_rate, volatility):
-    self.current_price = current_price
-    self.strike_price = strike_price
-    self.time = time
-    self.interest_rate = interest_rate
-    self.volatility = volatility
+    def __init__(self, current_price, strike_price, time, interest_rate, volatility):
+        self.current_price = current_price
+        self.strike_price = strike_price
+        self.time = time
+        self.interest_rate = interest_rate
+        self.volatility = volatility
 
-  def calculate_prices(self) -> tuple[float, float]:
-    K = self.strike_price
-    t = self.time
-    r = self.interest_rate
-    S = self.current_price
-    sigma = self.volatility
-    d1 = (np.log(S / K) + (r + (sigma ** 2) / 2) * t) / (sigma * np.sqrt(t))
-    d2 = d1 - (sigma * np.sqrt(t))
-    call_price = S * norm.cdf(d1) - K * np.exp(-r * t) * norm.cdf(d2)
-    put_price = K * np.exp(-r * t) * norm.cdf(-d2) - S * norm.cdf(-d1)
-    return call_price, put_price
+    def calculate_prices(self) -> tuple[float, float]:
+        K = self.strike_price
+        t = self.time
+        r = self.interest_rate
+        S = self.current_price
+        sigma = self.volatility
+        d1 = (np.log(S / K) + (r + (sigma ** 2) / 2) * t) / (sigma * np.sqrt(t))
+        d2 = d1 - (sigma * np.sqrt(t))
+        call_price = S * norm.cdf(d1) - K * np.exp(-r * t) * norm.cdf(d2)
+        put_price = K * np.exp(-r * t) * norm.cdf(-d2) - S * norm.cdf(-d1)
+        return call_price, put_price
 
-  def _calculate_prices(self, S, sigma) -> tuple[float, float]:
-    """This one's for internal usage because the current price (S) and volatility (sigma) have to change.
-    
-    Only implemented in heatmap() to account for the variation in values. 
-    """
-    
-    K = self.strike_price
-    t = self.time
-    r = self.interest_rate
-    d1 = (np.log(S / K) + (r + (sigma ** 2) / 2) * t) / (sigma * np.sqrt(t))
-    d2 = d1 - (sigma * np.sqrt(t))
-    call_price = S * norm.cdf(d1) - K * np.exp(-r * t) * norm.cdf(d2)
-    put_price = K * np.exp(-r * t) * norm.cdf(-d2) - S * norm.cdf(-d1)
-    return call_price, put_price
+    def _calculate_prices(self, S, sigma) -> tuple[float, float]:
+        """This one's for internal usage because the current price (S) and volatility (sigma) have to change.
+        
+        Only implemented in heatmap() to account for the variation in values. 
+        """
+        
+        K = self.strike_price
+        t = self.time
+        r = self.interest_rate
+        d1 = (np.log(S / K) + (r + (sigma ** 2) / 2) * t) / (sigma * np.sqrt(t))
+        d2 = d1 - (sigma * np.sqrt(t))
+        call_price = S * norm.cdf(d1) - K * np.exp(-r * t) * norm.cdf(d2)
+        put_price = K * np.exp(-r * t) * norm.cdf(-d2) - S * norm.cdf(-d1)
+        return call_price, put_price
 
-  def heatmap(self):
-    ARRAY_LENGTH = 10
-    RANGE = 0.2
-    call_2darray = np.zeros((ARRAY_LENGTH, ARRAY_LENGTH))
-    put_2darray = np.zeros((ARRAY_LENGTH, ARRAY_LENGTH))
-    S_range = np.linspace(start=(1-RANGE)*self.current_price, stop=(1+RANGE)*self.current_price, num=ARRAY_LENGTH)
-    sigma_range = np.linspace(start=(1-RANGE)*self.volatility, stop=(1+RANGE)*self.volatility, num=ARRAY_LENGTH)
-    
-    for i, S in enumerate(S_range):
-      for j, sigma in enumerate(sigma_range):
-        call, put = self._calculate_prices(S, sigma)
-        call_2darray[i][j] = call
-        put_2darray[i][j] = put
+    def heatmap(self):
+        ARRAY_LENGTH = 10
+        RANGE = 0.2
+        call_2darray = np.zeros((ARRAY_LENGTH, ARRAY_LENGTH))
+        put_2darray = np.zeros((ARRAY_LENGTH, ARRAY_LENGTH))
+        S_range = np.linspace(start=(1-RANGE)*self.current_price, stop=(1+RANGE)*self.current_price, num=ARRAY_LENGTH)
+        sigma_range = np.linspace(start=(1-RANGE)*self.volatility, stop=(1+RANGE)*self.volatility, num=ARRAY_LENGTH)
+        
+        for i, S in enumerate(S_range):
+            for j, sigma in enumerate(sigma_range):
+                call, put = self._calculate_prices(S, sigma)
+                call_2darray[i][j] = call
+                put_2darray[i][j] = put
 
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+        fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 
-    sns.heatmap(call_2darray, cmap="viridis", annot=True, ax=axes[0], fmt=".2f", annot_kws={"size": 5},
-                xticklabels=[str(x) for x in np.round(sigma_range, decimals=2)],
-                yticklabels=[str(y) for y in np.round(S_range, 2)])
-    axes[0].set_title("Call Values")
-    axes[0].set_xlabel("Volatility")
-    axes[0].set_ylabel("Current Price")
-    
-    sns.heatmap(put_2darray, cmap="viridis", annot=True, ax=axes[1], fmt=".2f", annot_kws={"size": 5},
-                xticklabels=[str(x) for x in np.round(sigma_range, decimals=2)],
-                yticklabels=[str(y) for y in np.round(S_range, 2)])
-    axes[1].set_title("Put Values")
-    axes[1].set_xlabel("Volatility")
-    axes[1].set_ylabel("Current Price")
-    
-    return fig
+        sns.heatmap(call_2darray, cmap="viridis", annot=True, ax=axes[0], fmt=".2f", annot_kws={"size": 5},
+                    xticklabels=[str(x) for x in np.round(sigma_range, decimals=2)],
+                    yticklabels=[str(y) for y in np.round(S_range, 2)])
+        axes[0].set_title("Call Values")
+        axes[0].set_xlabel("Volatility")
+        axes[0].set_ylabel("Current Price")
+        
+        sns.heatmap(put_2darray, cmap="viridis", annot=True, ax=axes[1], fmt=".2f", annot_kws={"size": 5},
+                    xticklabels=[str(x) for x in np.round(sigma_range, decimals=2)],
+                    yticklabels=[str(y) for y in np.round(S_range, 2)])
+        axes[1].set_title("Put Values")
+        axes[1].set_xlabel("Volatility")
+        axes[1].set_ylabel("Current Price")
+        
+        return fig
--- src/app.py
+++ src/app.py
@@ -3,50 +3,50 @@ import pandas as pd
 from BlackScholes import BlackScholes
 
 def main():
-  st.set_page_config(
-    page_title="Black-Scholes Option Pricing Model",
-    page_icon="📈",
-    layout="wide")
-  st.title("Black-Scholes Option Pricing")
-  
-  col1, col2, col3, col4, col5 = st.columns(5)
-  with col1:
-    current_price = st.number_input("Current Asset Price", value=100.0, min_value=1e-6)
-  with col2:
-    strike = st.number_input("Strike Price", value=100.0, min_value=1e-6)
-  with col3:
-    time = st.number_input("Time to Maturity (Years)", value=1.0, min_value=1e-6)
-  with col4:
-    volatility = st.number_input("Volatility (σ)", value=0.2, min_value=1e-6)
-  with col5:
-    interest_rate = st.number_input("Risk-Free Interest Rate", value=0.05)
+    st.set_page_config(
+        page_title="Black-Scholes Option Pricing Model",
+        page_icon="📈",
+        layout="wide")
+    st.title("Black-Scholes Option Pricing")
 
-  model = BlackScholes(current_price, strike, time, interest_rate, volatility)
-  call, put = model.calculate_prices()
-  c1, c2 = st.columns(2)
-  with c1: 
-    st.metric("Call", f"{call:.2f}")
-  with c2: 
-    st.metric("Put", f"{put:.2f}")
-    
-  st.subheader("Options Price - Heatmap")
-  st.pyplot(model.heatmap())
-  
-  st.subheader("Explanation for Black-Scholes")
-  df = pd.DataFrame(
-    {
-      "Variable": ["C", "N", "S", "K", "r", "t", "σ"],
-      "Description": ["Theoretical price of a European call option (the output)", 
-                      "CDF of the normal distribution",
-                      "Current asset price (spot price)",
-                      "Strike price",
-                      "Risk free interest rate",
-                      "Time to maturity",
-                      "Volatility of asset"]
-    }
-  )
-  st.dataframe(df.set_index("Variable"))
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        current_price = st.number_input("Current Asset Price", value=100.0, min_value=1e-6)
+    with col2:
+        strike = st.number_input("Strike Price", value=100.0, min_value=1e-6)
+    with col3:
+        time = st.number_input("Time to Maturity (Years)", value=1.0, min_value=1e-6)
+    with col4:
+        volatility = st.number_input("Volatility (σ)", value=0.2, min_value=1e-6)
+    with col5:
+        interest_rate = st.number_input("Risk-Free Interest Rate", value=0.05)
+
+    model = BlackScholes(current_price, strike, time, interest_rate, volatility)
+    call, put = model.calculate_prices()
+    c1, c2 = st.columns(2)
+    with c1: 
+        st.metric("Call", f"{call:.2f}")
+    with c2: 
+        st.metric("Put", f"{put:.2f}")
+        
+    st.subheader("Options Price - Heatmap")
+    st.pyplot(model.heatmap())
+
+    st.subheader("Explanation for Black-Scholes")
+    df = pd.DataFrame(
+        {
+        "Variable": ["C", "N", "S", "K", "r", "t", "σ"],
+        "Description": ["Theoretical price of a European call option (the output)", 
+                        "CDF of the normal distribution",
+                        "Current asset price (spot price)",
+                        "Strike price",
+                        "Risk free interest rate",
+                        "Time to maturity",
+                        "Volatility of asset"]
+        }
+    )
+    st.dataframe(df.set_index("Variable"))
 
 
 if __name__ == "__main__":
-  main()
+    main()
